452fc71ab5af271bee58c9d01b79d1758959fac8
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-07-04 09:10:21 -0700

    added answer B-1

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Table from './Table';
 
+export const AppContext = createContext(null);
+
 const App = () => {
+  const [repoDatas, setRepoDatas] = useState([]);
+
+  const getDatas = async () => {
+    await axios
+      .get('http://localhost:4000/repos/api')
+      .then((res) => {
+        setRepoDatas(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getDatas();
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Table />} />
-      </Routes>
-    </Router>
+    <AppContext.Provider value={{ repoDatas }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Table />} />
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
 };
 
