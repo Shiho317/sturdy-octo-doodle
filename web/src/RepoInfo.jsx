fa19c24ea34e6f47ab21fc3a7e26bfdb510207a7
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-07-04 10:05:32 -0700

    added answer B-7

--- web/src/RepoInfo.jsx
+++ web/src/RepoInfo.jsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from './App';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 const RepoInfo = () => {
   const params = useParams();
@@ -9,6 +12,7 @@ const RepoInfo = () => {
 
   const [clickedRepo, setClickedRepo] = useState([]);
   const [events, setEvents] = useState();
+  const [readMe, setReadMe] = useState();
 
   useEffect(() => {
     const repo = repoDatas.filter((data) => data.id === Number(params.id));
@@ -22,8 +26,19 @@ const RepoInfo = () => {
         .catch((err) => {
           console.log(err);
         });
+      axios
+        .get(
+          `https://raw.githubusercontent.com/${repo[0].full_name}/master/README.md`
+        )
+        .then((res) => {
+          setReadMe(res.data);
+        })
+        .catch((err) => {
+          console.log('Read Me for this repository is not exist.');
+        });
     }
   }, [params.id, repoDatas]);
+
   return (
     <>
       {clickedRepo.length > 0 && events && (
@@ -35,6 +50,16 @@ const RepoInfo = () => {
             <h3>author: {events.payload.commits[0].author.name}</h3>
             <h3>message: "{events.payload.commits[0].message}"</h3>
           </div>
+          {readMe && (
+            <div>
+              <h2>ReadMe</h2>
+              <ReactMarkdown
+                children={readMe}
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw]}
+              />
+            </div>
+          )}
         </div>
       )}
     </>
