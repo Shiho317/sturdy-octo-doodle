9f6dc773241fba319c12d0dec0bb24c1c45a17eb
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-07-04 08:34:07 -0700

    added answer A-1, A-2

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from 'express';
+import { Repo } from '../models/Repo';
+import axios from 'axios';
+import console from 'console';
 
 export const repos = Router();
 
@@ -9,4 +12,18 @@ repos.get('/', async (_: Request, res: Response) => {
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   res.json([]);
+});
+
+repos.get('/api', async (_: Request, res: Response) => {
+  res.header('Cache-Control', 'no-store');
+
+  await axios
+    .get<Repo[]>('https://api.github.com/users/silverorange/repos')
+    .then((result) => {
+      const { data } = result;
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
